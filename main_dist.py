8dc205c9e03798c58eb12bc0a7ad70fa97dcaafb
bdrhn9 <eee.bedirhan@gmail.com>
2021-04-24 18:49:01 +0300

    distributed mode only supported w/ DC-EPCC

--- main_dist.py
+++ main_dist.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
@@ -6,20 +5,17 @@ import datetime
 import os
 import time
 import json
-import glob
 import numpy as np
 import torch
 import torch.utils.data as D
 import torchvision.datasets as datasets
 import torch.nn.functional as F
-from torch import nn
-import torchvision
 from torchvision import transforms as T
 from backbones.model_irse import IR_50
 import modules.utils_torchvision as utils
-from modules.utils_mine import accuracy_l2, get_l2_pred
+
 from modules.utils_faceevolve import separate_irse_bn_paras, perform_val, get_val_pair,make_weights_for_balanced_classes
-from torch.utils.tensorboard import SummaryWriter
+
 from heads.epcc import DC_EPCC
 from losses.hinge_loss import hinge_mc_v3
 from losses.center_reg import center_regressor
@@ -29,8 +25,8 @@ os.environ['CUDA_DEVICE_ORDER'] = 'PCI_BUS_ID'
 os.environ['CUDA_VISIBLE_DEVICES'] = '0,1,2,3,4,5,6,7'
 os.environ["OMP_NUM_THREADS"]=str(3)
 
-def train_one_epoch(model, head, criterion, criterion_center, optimizer, optimizer_center, data_loader, device, epoch, args):
-    model.train(), head.train(), criterion.train(), criterion_center.train()
+def train_one_epoch(backbone, head, criterion, criterion_center, optimizer, optimizer_center, data_loader, device, epoch, args):
+    backbone.train(), head.train(), criterion.train(), criterion_center.train()
     metric_logger = utils.MetricLogger(delimiter="  ")
     metric_logger.add_meter('lr', utils.SmoothedValue(window_size=1, fmt='{value}'))
     metric_logger.add_meter('img/s', utils.SmoothedValue(window_size=10, fmt='{value:.1f}'))
@@ -44,24 +40,34 @@ def train_one_epoch(model, head, criterion, criterion_center, optimizer, optimiz
         else:
             centers = criterion_center.centers
        
-        features = model(image)
+        features = backbone(image)
+        outputs = head(features,centers)
+        
+        loss = criterion(outputs,target)
+        gama_reg_loss = head.get_gama_reg_loss()
+        
+        center_reg_loss = criterion_center(features,target)
 
-        if(epoch>args.only_centers_upto):
-            optimizer.zero_grad()
-            optimizer_center.zero_grad()
-            loss.backward()
-            optimizer.step()
-            optimizer_center.step()
-        else:
-            optimizer_center.zero_grad()
-            loss.backward()
-            optimizer_center.step()
+        # epccs' gama regularization
+        if(args.kapa != 0.0):
+            loss += gama_reg_loss
+            
+        optimizer_center.zero_grad()
+        optimizer.zero_grad()
+        loss.backward(retain_graph=True)
+        optimizer.step()
         
-        acc1 = accuracy_l2(features,centers,target[:,0])
-        
+        optimizer_center.zero_grad()
+        optimizer.zero_grad()
+        center_reg_loss.backward()
+        optimizer_center.step()
+            
+        acc1, acc5 = utils.accuracy(outputs, target, topk=(1, 5))
+ 
         batch_size = image.shape[0]
-        metric_logger.update(loss=loss.item(),interclass_center_loss=interclass_center_loss.item(),intraclass_loss=intraclass_center_loss.item(),interclass_triplet=interclass_triplet.item(),lr=optimizer.param_groups[0]["lr"])
+        metric_logger.update(loss=loss.item(), gama_reg_loss=gama_reg_loss.item(), center_reg_loss = center_reg_loss.item(),lr=optimizer.param_groups[0]["lr"])
         metric_logger.meters['acc1'].update(acc1.item(), n=batch_size)
+        metric_logger.meters['acc5'].update(acc5.item(), n=batch_size)
         metric_logger.meters['img/s'].update(batch_size / (time.time() - start_time))
     
     metric_logger.synchronize_between_processes()
@@ -118,25 +124,25 @@ def main(args):
         dataset, batch_size=args.batch_size,
         sampler=train_sampler, num_workers=args.workers, pin_memory=True,drop_last=True)
 
-    print("Creating model")
+    print("Creating backbone")
 
-    model = IR_50(pretrained=True,input_size=[112,112])
+    backbone = IR_50(pretrained=True,input_size=[112,112])
     #model = resnet50(pretrained=True,input_size=112,vis2d=False)
-    model.to(device)
+    backbone.to(device)
 
-    head = DC_EPCC(model.embed_size,len(dataset.classes),kapa=args.kapa)
+    head = DC_EPCC(backbone.embed_size,len(dataset.classes),kapa=args.kapa)
     head.to(device)
 
     criterion = hinge_mc_v3(args.margin)
     criterion.to(device)
 
-    criterion_center = center_regressor(model.embed_size,len(dataset.classes),mutex_label=False)
+    criterion_center = center_regressor(backbone.embed_size,len(dataset.classes),mutex_label=False)
     criterion_center.to(device)
     
     if args.distributed and args.sync_bn:
-        model = torch.nn.SyncBatchNorm.convert_sync_batchnorm(model)
+        backbone = torch.nn.SyncBatchNorm.convert_sync_batchnorm(backbone)
 
-    backbone_paras_only_bn, backbone_paras_wo_bn = separate_irse_bn_paras(model) # separate batch_norm parameters from others; do not do weight decay for batch_norm parameters to improve the generalizability
+    backbone_paras_only_bn, backbone_paras_wo_bn = separate_irse_bn_paras(backbone) # separate batch_norm parameters from others; do not do weight decay for batch_norm parameters to improve the generalizability
 
     optimizer = torch.optim.Adam([{'params': backbone_paras_wo_bn+head.parse_params()['w'], 'weight_decay': args.weight_decay}, {'params': backbone_paras_only_bn+head.parse_params()['wabs']}], lr = args.lr)
     #optimizer = torch.optim.SGD([{'params': backbone_paras_wo_bn+head.parse_params()['w'], 'weight_decay': args.weight_decay}, {'params': backbone_paras_only_bn+head.parse_params()['wabs']}], lr = args.lr, momentum = args.momentum)
@@ -145,14 +151,14 @@ def main(args):
     
     lr_scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer,
                                                     milestones=[int(args.epochs*0.6),int(args.epochs*0.8)])
-    model_without_ddp = model
+    backbone_without_ddp = backbone
     head_without_ddp = head
     criterion_without_ddp = criterion
     criterion_center_without_ddp = criterion_center
 
     if args.distributed:
-        model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[args.gpu])
-        model_without_ddp = model.module
+        backbone = torch.nn.parallel.DistributedDataParallel(backbone, device_ids=[args.gpu])
+        backbone_without_ddp = backbone.module
         
         head = torch.nn.parallel.DistributedDataParallel(head, device_ids=[args.gpu])
         head_without_ddp = head.module
@@ -167,7 +173,7 @@ def main(args):
     
     if args.resume:
         checkpoint = torch.load(args.resume, map_location='cpu')
-        model_without_ddp.load_state_dict(checkpoint['model'])
+        backbone_without_ddp.load_state_dict(checkpoint['backbone'])
         head_without_ddp.load_state_dict(checkpoint['head'])
         criterion_without_ddp.load_state_dict(checkpoint['criterion'])
         criterion_center_without_ddp.load_state_dict(checkpoint['criterion_center'])
@@ -179,7 +185,7 @@ def main(args):
         best_val_acc1_norm_lfw = checkpoint['best_val_acc1_norm_lfw']
 
     if args.test_only:
-        print(perform_val(device, 512, 128, model, lfw, lfw_issame,normalized=True))
+        print(perform_val(device, 512, 128, backbone, lfw, lfw_issame,normalized=True))
 
     print("Start training")
     start_time = time.time()
@@ -187,25 +193,25 @@ def main(args):
         if args.distributed:
             train_sampler.set_epoch(epoch)
         
-        acc1, loss = train_one_epoch(model, optimizer, criterion_center, optimizer_center, data_loader, device, epoch, dataset.num_subset, dataset.num_subcluster, args)
+        acc1, loss = train_one_epoch(backbone, optimizer, criterion_center, optimizer_center, data_loader, device, epoch, dataset.num_subset, dataset.num_subcluster, args)
         
         if(epoch>args.only_centers_upto):
             lr_scheduler.step()
         
         if(epoch%args.eval_freq == 0 and epoch!=0):
             
-            val_acc1_norm_lfw, _best_threshold_norm_lfw, _roc_curve_norm_lfw = perform_val(device, 512, 128, model, lfw, lfw_issame,normalized=True)
+            val_acc1_norm_lfw, _best_threshold_norm_lfw, _roc_curve_norm_lfw = perform_val(device, 512, 128, backbone, lfw, lfw_issame,normalized=True)
             is_best_norm_lfw = val_acc1_norm_lfw > best_val_acc1_norm_lfw
             best_val_acc1_norm_lfw = max(val_acc1_norm_lfw, best_val_acc1_norm_lfw)
             print('VAL_ACC1_NORMALIZED_LFW: %.5f, BEST_VAL_ACC1_NORMALIZED_LFW: %.5f '%(val_acc1_norm_lfw,best_val_acc1_norm_lfw))
 
-            val_acc1_lfw, _best_threshold_lfw, _roc_curve_lfw = perform_val(device, 512, 128, model, lfw, lfw_issame,normalized=False)
+            val_acc1_lfw, _best_threshold_lfw, _roc_curve_lfw = perform_val(device, 512, 128, backbone, lfw, lfw_issame,normalized=False)
             is_best_lfw = val_acc1_lfw > best_val_acc1_lfw
             best_val_acc1_lfw = max(val_acc1_lfw, best_val_acc1_lfw)
             print('VAL_ACC1_LFW: %.5f, BEST_VAL_ACC1_LFW: %.5f '%(val_acc1_lfw,best_val_acc1_lfw))
 
             checkpoint = {
-                'model': model_without_ddp.state_dict(),
+                'backbone': backbone_without_ddp.state_dict(),
                 'head': head_without_ddp.state_dict(),
                 'criterion':criterion_without_ddp.state_dict(),
                 'criterion_center': criterion_center_without_ddp.state_dict(),
@@ -246,8 +252,6 @@ def parse_args():
                         help='start epoch')
     parser.add_argument('--epochs', default=751, type=int, metavar='N',
                         help='number of total epochs to run')
-    parser.add_argument('--only-centers-upto', default=-1, type=int, metavar='N',
-                        help='train only centers up to epoch #')
     parser.add_argument('-j', '--workers', default=3, type=int, metavar='N',
                         help='number of data loading workers (default: 16)')
     parser.add_argument('--lr', default=0.00001, type=float, help='initial learning rate')
@@ -258,6 +262,7 @@ def parse_args():
                         dest='weight_decay')
     parser.add_argument('--lr-step-size', default=350, type=int, help='decrease lr every step-size epochs')
     parser.add_argument('--lr-gamma', default=0.1, type=float, help='decrease lr by a factor of lr-gamma')
+    parser.add_argument('--margin', default=1.0, type=float, help='decrease lr by a factor of lr-gamma')
     parser.add_argument('--kapa', default=1.0, type=float, help='decrease lr by a factor of lr-gamma')
     parser.add_argument('--print-freq', default=200, type=int, help='print frequency')
     parser.add_argument('--eval-freq', default=30, type=int, help='print frequency')
